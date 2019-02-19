48603be0881e738532b86605f37c60ae30d94588
Evan Canoy <evan@Evans-MacBook-Pro.local>
2019-02-19 16:39:46 -0500

    updated index.js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const express = require('express')
 const parser = require('body-parser')
 const cors = require('cors')
+const app = express()
+
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.use(require('./Routes/indexRoutes.js'))
+
+
+app.listen('6000',(req, res) => {
+    console.log('listening on port 6000')
+} )
