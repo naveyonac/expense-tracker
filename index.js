d41cf18ac73f9882ddab56de7c7686af7e276481
Fathia Mohammed <mohammedfo5429@gmail.com>
2019-02-20 11:59:50 -0500

    working on index

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const cors = require('cors')
+const mongoose = require('./Models/Revenue')
+const Revenue = mongoose.model('Revenue')
+
+// const cors = require('cors')
 const app = express()
+
 
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use(require('./Routes/indexRoutes'))
+
+
+
+app.get("/", (req, res) => {
+    // response.send("Hello WOorld")
+
+   
+        gif.find()
+          .then((gifs) => {
+            res.json(gifs)
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+
+   } )
 
 
 app.listen(1000,(req, res) => {
