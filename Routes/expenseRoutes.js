9e1c90ac3b2f831365a2c6b60dd071c7d5a7bb0d
Fathia Mohammed <mohammedfo5429@gmail.com>
2019-02-20 13:18:46 -0500

    got both routes to render

--- Routes/expenseRoutes.js
+++ Routes/expenseRoutes.js
@@ -3,4 +3,11 @@ const router = express.Router()
 const mongoose = require('./../Models/Expense')
 const Expense = mongoose.model('Expense')
 
+
+
+router.get('/expense', (req, res) => {
+    Expense.find().then(result => {
+        res.json(result)
+    })
+})
 module.exports = router
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const parser = require('body-parser')
-const mongoose = require('./Models/Revenue')
-const Revenue = mongoose.model('Revenue')
+// const mongoose = require('./Models/Revenue')
+const Revenue = require('./Models/Revenue')
+
+// const ExpenseMongoose = require('./Models/Expense')
+const Expense = require('./Models/Expense')
+
+// const Expense = ExpenseMongoose.model('Revenue')
+// const Revenue = mongoose.model('Revenue')
 
 // const cors = require('cors')
 const app = express()
@@ -12,6 +18,7 @@ app.use(parser.json())
 
 app.use(require('./Routes/indexRoutes'))
 
+// app.use(require('./Routes/indexRoutes'))
 
 
 app.get("/revenue", (req, res) => {
@@ -28,6 +35,20 @@ app.get("/revenue", (req, res) => {
 
    } )
 
+
+   app.get("/expense", (req, res) => {
+    // response.send("Hello WOorld")
+
+   
+    Expense.find()
+          .then((expense) => {
+            res.json(expense)
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+
+   } )
 
 app.listen(1000,(req, res) => {
     console.log('listening on port 1000')
